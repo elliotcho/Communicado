b1778782522af51754baa6912430ba176010a6e2
Yong <ycho97@uwo.ca>
2020-06-17 22:51:20 -0400

    fourth commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {HashRouter, Route} from 'react-router-dom';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
+import Home from './Pages/Home/Home';
 
 class App extends Component{
     render(){
@@ -9,6 +10,7 @@ class App extends Component{
          <HashRouter>
            <Route exact path='/' component={Login}/>
            <Route exact path='/signup' component={Signup}/>
+           <Route exact path='/home' component={Home}/>
          </HashRouter>
       )
     }
