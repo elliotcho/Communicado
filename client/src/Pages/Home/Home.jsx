3021c2d649091356782bc5801d440418cfe6a604
Yong <ycho97@uwo.ca>
2020-06-24 21:18:36 -0400

    partials refactor

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './Home.css';
-import Navbar from '../../Components/Navbar'
+import Navbar from '../../Partials/Navbar'
 
 import loading from './loading.jpg';
 
