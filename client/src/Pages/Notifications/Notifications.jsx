ebb4b4c0ca70304bfe9ab67da7deed9d5975ac13
Yong <ycho97@uwo.ca>
2020-06-29 22:47:30 -0400

    notifs

--- client/src/Pages/Notifications/Notifications.jsx
+++ client/src/Pages/Notifications/Notifications.jsx
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
 import Navbar from '../../Partials/Navbar';
+import './Notifications.css'
+import loading from './loading.jpg';
 
 const axios=require('axios');
 
 class Notifications extends Component{
     render(){
         return(
-            <div className="notifications">
+            <div className="notifs">
                 <Navbar/>
 
                 <header>
                     <h1>Notifications</h1>
                 </header>
 
+	            <h4>Latest</h4>
+
+                <div className='row'>
+                    <img src={loading} alt='profile'/>
+        
+                    <div>
+                        <p>
+                            <strong>Gugsa Challa</strong>
+                            <span> liked your picture!</span>
+                        </p>
+                    </div>
+                </div>
             </div>
         )
     }
--- client/src/Partials/Navbar.jsx
+++ client/src/Partials/Navbar.jsx
@@ -19,7 +19,7 @@ class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed">
                     <div className="container-fluid">
-                        <a className="navbar-brand">Communicado</a>
+                        <Link to='/home' className="navbar-brand">Communicado</Link>
                         
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
                             <span className="navbar-toggler-icon"></span>
@@ -36,24 +36,24 @@ class Navbar extends Component {
                                 </li>
                                 <li>
                                     <Link to='/friends'>
-                                        <a className="nav-link pr-lg-5">Friends</a>
+                                        <a href='/friends' className="nav-link pr-lg-5">Friends</a>
                                     </Link>
                                 </li>
                                 <li>
                                     <Link to='/notifications' className="nav-link">
-                                        <a className="nav-link d-inline-block d-md-none">Notifications</a>
+                                        <a href='/notifications' className="nav-link d-inline-block d-md-none">Notifications</a>
                                         <i class="fas fa-bell"></i>
                                     </Link>
                                 </li>
                                 <li>
                                     <Link to='/settings' className="nav-link">
-                                        <a className="nav-link d-inline-block d-md-none">Settings</a>
+                                        <a href='/settings' className="nav-link d-inline-block d-md-none">Settings</a>
                                         <i class="fas fa-user-cog"></i>
                                     </Link>
                                 </li>
                                 <li>
-                                    <a onClick={this.signOut} class='nav-link'>
-                                        <a className="nav-link d-inline-block d-md-none">Logout</a>
+                                    <a href='/' onClick={this.signOut} class='nav-link'>
+                                        <a href='/' onClick={this.signOut} className="nav-link d-inline-block d-md-none">Logout</a>
                                         <i class="fas fa-sign-out-alt"></i>
                                     </a>
                                 </li>
