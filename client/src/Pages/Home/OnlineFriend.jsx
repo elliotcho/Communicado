d7314daf4485f4f6577ef6bb11c6d22ad1bc6681
ericywang <ywericc@gmail.com>
2020-07-30 23:27:19 -0400

    home front end

---
+++ client/src/Pages/Home/OnlineFriend.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import './OnlineFriend.css'
+import avatar from './avatar6.png';
+
+class OnlineFriend extends Component {
+    render() {
+        return (
+
+            <div class="row sideBar-body">
+                
+                <div class="col-sm-3 sideBar-avatar">
+                    <div class="avatar-icon">
+                        <img src={avatar}></img>
+                    </div>
+                </div>
+                <div class="col-sm-9 sideBar-main">
+                    <div class="row">
+                        <div class="col-sm-8 sideBar-name">
+                            <span class="name-meta">John Doe
+                            </span>
+                        </div>
+                        <div class="col-sm-4 pull-right sideBar-time">
+                            <span class="time-meta pull-right">Last Chatted 18:18
+                            </span>
+                        </div>  
+                    </div>
+                </div>
+                
+            </div>
+        )
+    }
+}
+export default OnlineFriend
