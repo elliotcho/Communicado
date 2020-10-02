de62b6a12af4c8aff447256d10007087b45a6970
Yong <ycho97@uwo.ca>
2020-10-02 16:42:22 -0400

    test environment removed

--- server/routes/user.js
+++ server/routes/user.js
@@ -22,13 +22,4 @@ router.post('/changepwd', changePwd);
 router.delete('/:uid', deleteUser);
 router.post('/search', findUsers);
 
-const {User} = require('../models/user');
-router.post('/test', async (req, res) => {
-    const {uid, pwd} = req.body;
-
-    await User.updateOne({_id: uid}, {password: pwd});
-
-    res.json({msg: "DONE"});
-});
-
 module.exports = router;
